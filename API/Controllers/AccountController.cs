da8fe16a7cb616975b4edc6e7e7e3a4051208cc4
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2022-12-27 14:10:01 -0500

    Added login endpoint and LoginDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,6 +38,26 @@ namespace API.Controllers
             
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            if(user == null)
+            {
+                return Unauthorized("Invalid Username");
+            }
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+            for(int i = 0; i < computedHash.Length; i++) 
+            {
+                if (computedHash[i] != user.PasswordHash[i]) 
+                {
+                    return Unauthorized("Invalid Password");
+                }
+            }
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
