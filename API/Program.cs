622f2f5d83f092a0e4a189783d8d0e276b00df88
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2022-12-27 13:56:20 -0500

    Created simple user register API and RegisterDTO

--- API/Program.cs
+++ API/Program.cs
@@ -10,13 +10,13 @@ builder.Services.AddDbContext<DataContext>(option =>
 {
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
