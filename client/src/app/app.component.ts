8bc910c7862ee8bc8914d0173201384c10951da0
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2022-12-28 12:59:03 -0500

    Added login persistence with local storage, will update to better practice soon

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +11,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title: string = 'Dating App';
   users: any;
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private accountService: AccountService){
 
   }
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers(){
     this.http.get('https://localhost:5001/api/users/').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
@@ -21,4 +28,10 @@ export class AppComponent implements OnInit {
     });  
   }
 
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 }
